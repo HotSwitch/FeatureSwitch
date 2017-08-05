a04b4a6960955bc367b06a5b7715f1e7d286fbf6
Jonathan Andrade <jonathan_andrade91@hotmail.com>
2017-08-05 02:55:58 -0400

    Cocoa framework added

---
+++ Example/FeatureSwitch/FeatureSwitch.h
@@ -0,0 +1,19 @@
+//
+//  FeatureSwitch.h
+//  FeatureSwitch
+//
+//  Created by Jonathan Andrade on 02/08/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FeatureSwitch.
+FOUNDATION_EXPORT double FeatureSwitchVersionNumber;
+
+//! Project version string for FeatureSwitch.
+FOUNDATION_EXPORT const unsigned char FeatureSwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeatureSwitch/PublicHeader.h>
+
+
